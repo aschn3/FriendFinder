f901a1197f0877ecf641f68e937d7c8142f047fc
ANDREW SCHNARRE <ANDREW SCHNARRE>
2017-04-29 13:35:17 -0400

    aonther update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -16,24 +17,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "andy",
-  password: "password123",
-  database: "friend_finder_db"
-});
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-
-
-
+  
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
